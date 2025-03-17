b1c6d54838c51a8757c4acfcf00d18fc44853fd3
oleg425top <oleg59152@gmail.com>
2025-03-17 14:38:24 +0500

    commit2

--- 03_DB_HH/DB_Operator/DB_Operator.py
+++ 03_DB_HH/DB_Operator/DB_Operator.py
@@ -22,6 +22,10 @@ class ConnectDB:
         else:
             return conn
 
+    @classmethod
+    def close_connection(cls, conn_obj):
+        conn_obj.close()
+
 
 class MSSQLOperator:
 
@@ -35,29 +39,59 @@ class MSSQLOperator:
             self.conn.execute(SQLCommand)
         except pyodbc.ProgrammingError as ex:
             print(ex)
-            self.conn.close()
+
             return False
         else:
-            self.conn.close()
+
             print(f'База данных {database_name} успешно создана')
 
-
-    def create_table(self, database_name, table_name, sql_query):
+    def create_table(self, database_name, sql_query, table_name, references_table=None, references_column=None):
         self.conn_cursor.execute(f'USE {database_name}')
-        SQLQuery = sql_query(table_name)
+        SQLQuery = sql_query(table_name, references_table, references_column)
         try:
             self.conn_cursor.execute(SQLQuery)
         except pyodbc.ProgrammingError as exPE:
             print(exPE)
-            self.conn.close()
+
             return False
         except pyodbc.OperationalError as exOE:
             print(exOE)
-            self.conn.close()
+
             return False
         else:
             print(f'Таблица {table_name} успешно создана!')
-            self.conn.close()
+
+            return True
+
+    def drop_table(self, database_name, sql_query, table_name):
+        self.conn_cursor.execute(f'USE {database_name}')
+        SQL_Query = sql_query(table_name)
+        try:
+            self.conn_cursor.execute(SQL_Query)
+        except pyodbc.Error as ex:
+            print(ex)
+            return False
+        else:
+            print(f'Таблица {table_name} успешно удалена!')
             return True
 
 
+if __name__ == '__main__':
+    load_dotenv()
+
+    DRIVER = os.getenv('MS_SQL_DRIVER')
+    SERVER = os.getenv('MS_SQL_SERVER')
+    WORK_DATABASE = "EmployersVacancies425"
+    PAD_DATABASE = os.getenv('MS_PAD_DATABASE')
+    USER = os.getenv('MS_SQL_USER')
+    PASSWORD = os.getenv('MS_SQL_KEY')
+
+    my_conn = ConnectDB.connect_to_db(driver=DRIVER, server=SERVER, pad_database=PAD_DATABASE, user=USER,
+                                      password=PASSWORD)
+    my_db_operator = MSSQLOperator(my_conn)
+    my_db_operator.create_database_params(WORK_DATABASE, '10', '20', '5%')
+    my_db_operator.drop_table(WORK_DATABASE, SQL_Queries.drop_table, 'Vacancies')
+    my_db_operator.drop_table(WORK_DATABASE, SQL_Queries.drop_table, 'Employers')
+    my_db_operator.create_table(WORK_DATABASE, SQL_Queries.create_table_employers, 'Employers')
+    my_db_operator.create_table(WORK_DATABASE, SQL_Queries.create_table_vacancies, table_name='Vacancies',
+                                references_table='Employers', references_column='id')
--- 03_DB_HH/DB_Operator/SQL_Queries.py
+++ 03_DB_HH/DB_Operator/SQL_Queries.py
@@ -4,16 +4,42 @@ def create_database_default(name):
 
 
 def create_database(name, size, maxsize, filegrowth):
-    pass
+    QUERY = fr"""
+    CREATE DATABASE {name}
+    ON
+    (NAME = {name}Database_data,
+    FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\{name}Database_data.mdf',
+    SIZE = {size}MB,
+    MAXSIZE = {maxsize}GB,
+    FILEGROWTH = {filegrowth})
+    LOG ON
+    (NAME = {name}Database_log,
+    FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\{name}Database_data.ldf',
+    SIZE = {size}MB,
+    MAXSIZE = {str(round(int(maxsize) * 0.1))}GB,
+    FILEGROWTH = {filegrowth})
+    """
+    return QUERY
 
 
-def create_table_employers(table_name):
-    pass
+def create_table_employers(table_name, references_table =None, references_column=None):
+    QUERY = f"""CREATE TABLE {table_name}
+                (id int PRIMARY KEY,
+                employer_name nvarchar(200),
+                employer_url nvarchar(200));"""
+    return QUERY
 
-
-def create_table_vacancies(table_name):
-    pass
-
+def create_table_vacancies(table_name, references_table, references_column):
+    QUERY = f"""CREATE TABLE {table_name}
+                (id int PRIMARY KEY,
+                vacancy_city nvarchar(200),
+                vacancy_name nvarchar(200),
+                vacancy_url nvarchar(200),
+                vacancy_salary_from money,
+                vacancy_salary_to money,
+                employer_id int REFERENCES {references_table} ({references_column}));"""
+    return QUERY
 
 def drop_table(table_name):
-    pass
+    QUERY = f"DROP TABLE {table_name}"
+    return QUERY
