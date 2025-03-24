d9fffea78d3d37a7b2fa993b53b433c46be15b27
oleg425top <oleg59152@gmail.com>
2025-03-24 15:10:22 +0500

    All DONE!!!

--- 04_Home_work_Hospital/Hospital_Operator.py
+++ 04_Home_work_Hospital/Hospital_Operator.py
@@ -1,6 +1,9 @@
 import os
+import json
+
 import pyodbc
 from dotenv import load_dotenv
+
 from sql_query import sql_query
 
 import SQL_Queries
@@ -89,9 +92,10 @@ class MSSQLOperator:
                 print(rec)
             return True
 
-    def exists_query(self, database_name, sql_query):
+    def first_exists_query(self, database_name, sql_query):
         self.conn_cursor.execute(f'USE {database_name}')
         SQL_Query = sql_query()
+        data_list = []
         try:
             result = self.conn_cursor.execute(SQL_Query)
         except pyodbc.Error as ex:
@@ -99,13 +103,39 @@ class MSSQLOperator:
             return False
         else:
             records = result.fetchall()
-            for rec in records:
-                print(rec)
+            for record in records:
+                data_dict = {'DepartmentName': record.DepartmentName}
+                data_list.append(data_dict)
+            print(data_list)
+            with open('Json_data/first_exists_query.json', 'w', encoding='utf-8') as fp:
+                json.dump(data_list, fp, ensure_ascii=False, indent=4)
+                print('Data added to json file')
+            return True
+
+    def second_exists_query(self, database_name, sql_query):
+        self.conn_cursor.execute(f'USE {database_name}')
+        SQL_Query = sql_query()
+        data_list = []
+        try:
+            result = self.conn_cursor.execute(SQL_Query)
+        except pyodbc.Error as ex:
+            print(ex)
+            return False
+        else:
+            records = result.fetchall()
+            for record in records:
+                data_dict = {'FullName': record.FullName}
+                data_list.append(data_dict)
+            print(data_list)
+            with open('Json_data/second_exists_query.json', 'w', encoding='utf-8') as fp:
+                json.dump(data_list, fp, ensure_ascii=False, indent=4)
+                print('Data added to json file')
             return True
 
     def any_query(self, database_name, sql_query):
         self.conn_cursor.execute(f'USE {database_name}')
         SQL_Query = sql_query()
+        data_list = []
         try:
             result = self.conn_cursor.execute(SQL_Query)
         except pyodbc.Error as ex:
@@ -113,9 +143,176 @@ class MSSQLOperator:
             return False
         else:
             records = result.fetchall()
-            for rec in records:
-                print(rec)
+            for record in records:
+                data_dict = {'FullName': record.FullName}
+                data_list.append(data_dict)
+            print(data_list)
+            with open('Json_data/any_query.json', 'w', encoding='utf-8') as fp:
+                json.dump(data_list, fp, ensure_ascii=False, indent=4)
+                print('Data added to json file')
             return True
+
+    def some_query(self, database_name, sql_query):
+        self.conn_cursor.execute(f'USE {database_name}')
+        SQL_Query = sql_query()
+        data_list = []
+        try:
+            result = self.conn_cursor.execute(SQL_Query)
+        except pyodbc.Error as ex:
+            print(ex)
+            return False
+        else:
+            records = result.fetchall()
+            for record in records:
+                data_dict = {'Surname': record.Surname, 'Name':record.Name}
+                data_list.append(data_dict)
+            print(data_list)
+            with open('Json_data/some_query.json', 'w', encoding='utf-8') as fp:
+                json.dump(data_list, fp, ensure_ascii=False, indent=4)
+                print('Data added to json file')
+            return True
+
+    def all_query(self, database_name, sql_query):
+        self.conn_cursor.execute(f'USE {database_name}')
+        SQL_Query = sql_query()
+        data_list = []
+        try:
+            result = self.conn_cursor.execute(SQL_Query)
+        except pyodbc.Error as ex:
+            print(ex)
+            return False
+        else:
+            records = result.fetchall()
+            for record in records:
+                data_dict = {'Surname': record.Surname, 'Name':record.Name}
+                data_list.append(data_dict)
+            print(data_list)
+            with open('Json_data/all_query.json', 'w', encoding='utf-8') as fp:
+                json.dump(data_list, fp, ensure_ascii=False, indent=4)
+                print('Data added to json file')
+            return True
+
+    def any_some_all_query(self, database_name, sql_query):
+        self.conn_cursor.execute(f'USE {database_name}')
+        SQL_Query = sql_query()
+        data_list = []
+        try:
+            result = self.conn_cursor.execute(SQL_Query)
+        except pyodbc.Error as ex:
+            print(ex)
+            return False
+        else:
+            records = result.fetchall()
+            for record in records:
+                data_dict = {'Surname': record.Surname, 'Name':record.Name}
+                data_list.append(data_dict)
+            print(data_list)
+            with open('Json_data/any_some_all_query.json', 'w', encoding='utf-8') as fp:
+                json.dump(data_list, fp, ensure_ascii=False, indent=4)
+                print('Data added to json file')
+            return True
+
+    def union_query(self, database_name, sql_query):
+        self.conn_cursor.execute(f'USE {database_name}')
+        SQL_Query = sql_query()
+        data_list = []
+        try:
+            result = self.conn_cursor.execute(SQL_Query)
+        except pyodbc.Error as ex:
+            print(ex)
+            return False
+        else:
+            records = result.fetchall()
+            for record in records:
+                data_dict = {'Surname': record.Surname, 'Name':record.Name}
+                data_list.append(data_dict)
+            print(data_list)
+            with open('Json_data/union_query.json', 'w', encoding='utf-8') as fp:
+                json.dump(data_list, fp, ensure_ascii=False, indent=4)
+                print('Data added to json file')
+            return True
+
+    def union_all_query(self, database_name, sql_query):
+        self.conn_cursor.execute(f'USE {database_name}')
+        SQL_Query = sql_query()
+        data_list = []
+        try:
+            result = self.conn_cursor.execute(SQL_Query)
+        except pyodbc.Error as ex:
+            print(ex)
+            return False
+        else:
+            records = result.fetchall()
+            for record in records:
+                data_dict = {'Surname': record.Surname, 'Name':record.Name}
+                data_list.append(data_dict)
+            print(data_list)
+            with open('Json_data/union_all_query.json', 'w', encoding='utf-8') as fp:
+                json.dump(data_list, fp, ensure_ascii=False, indent=4)
+                print('Data added to json file')
+            return True
+
+    def inner_join(self, database_name, sql_query):
+        self.conn_cursor.execute(f'USE {database_name}')
+        SQL_Query = sql_query()
+        data_list = []
+        try:
+            result = self.conn_cursor.execute(SQL_Query)
+        except pyodbc.Error as ex:
+            print(ex)
+            return False
+        else:
+            records = result.fetchall()
+            for record in records:
+                data_dict = {'Surname': record.Surname, 'Name':record.Name, 'DepartmentName':record.DepartmentName}
+                data_list.append(data_dict)
+            print(data_list)
+            with open('Json_data/inner_join.json', 'w', encoding='utf-8') as fp:
+                json.dump(data_list, fp, ensure_ascii=False, indent=4)
+                print('Data added to json file')
+            return True
+
+    def left_join(self, database_name, sql_query):
+        self.conn_cursor.execute(f'USE {database_name}')
+        SQL_Query = sql_query()
+        data_list = []
+        try:
+            result = self.conn_cursor.execute(SQL_Query)
+        except pyodbc.Error as ex:
+            print(ex)
+            return False
+        else:
+            records = result.fetchall()
+            for record in records:
+                data_dict = {'Surname': record.Surname, 'Name':record.Name, 'DepartmentName':record.DepartmentName}
+                data_list.append(data_dict)
+            print(data_list)
+            with open('Json_data/left_join.json', 'w', encoding='utf-8') as fp:
+                json.dump(data_list, fp, ensure_ascii=False, indent=4)
+                print('Data added to json file')
+            return True
+
+    def right_join(self, database_name, sql_query):
+        self.conn_cursor.execute(f'USE {database_name}')
+        SQL_Query = sql_query()
+        data_list = []
+        try:
+            result = self.conn_cursor.execute(SQL_Query)
+        except pyodbc.Error as ex:
+            print(ex)
+            return False
+        else:
+            records = result.fetchall()
+            for record in records:
+                data_dict = {'Surname': record.Surname, 'Name':record.Name, 'DepartmentName':record.DepartmentName}
+                data_list.append(data_dict)
+            print(data_list)
+            with open('Json_data/right_join.json', 'w', encoding='utf-8') as fp:
+                json.dump(data_list, fp, ensure_ascii=False, indent=4)
+                print('Data added to json file')
+            return True
+
+
 
 
 if __name__ == '__main__':
@@ -135,8 +332,24 @@ if __name__ == '__main__':
     # table_name = input('Ведите название таблицы')
     # my_db_operator.create_table(WORK_DATABASE, SQL_Queries.create_table, table_name)
     # my_db_operator.drop_table(WORK_DATABASE, SQL_Queries.drop_table, table_name)
-    # my_db_operator.exists_query(WORK_DATABASE, SQL_Queries.first_exists_query)
+    my_db_operator.first_exists_query(WORK_DATABASE, SQL_Queries.first_exists_query)
     print()
-    # my_db_operator.exists_query(WORK_DATABASE, SQL_Queries.second_exists_query)
     # my_db_operator.get_data_from_any_table(WORK_DATABASE, SQL_Queries.universal_query)
+    my_db_operator.second_exists_query(WORK_DATABASE, SQL_Queries.second_exists_query)
     my_db_operator.any_query(WORK_DATABASE, SQL_Queries.any_query)
+    print()
+    my_db_operator.some_query(WORK_DATABASE, SQL_Queries.some_query)
+    print()
+    my_db_operator.all_query(WORK_DATABASE, SQL_Queries.all_query)
+    my_db_operator.any_some_all_query(WORK_DATABASE, SQL_Queries.any_some_all_query)
+    print()
+    my_db_operator.union_query(WORK_DATABASE, SQL_Queries.union_query)
+    print()
+    my_db_operator.union_all_query(WORK_DATABASE, SQL_Queries.union_all_query)
+    print()
+    my_db_operator.inner_join(WORK_DATABASE, SQL_Queries.inner_join)
+    print()
+    my_db_operator.left_join(WORK_DATABASE, SQL_Queries.left_join)
+    print()
+    my_db_operator.right_join(WORK_DATABASE, SQL_Queries.right_join)
+    print()
--- 04_Home_work_Hospital/SQL_Queries.py
+++ 04_Home_work_Hospital/SQL_Queries.py
@@ -1,3 +1,6 @@
+import json
+
+
 def create_table(table_name):
     # table_name = input('Ведите название таблицы')
     query_list = []
@@ -65,4 +68,127 @@ def any_query():
                 WHERE Salary > ANY(
                 SELECT AVG(Salary)
                 FROM Doctors);"""
+    return QUERY
+
+
+"""SOME. Врачи, у которых премия выше средней премии по всем отделам."""
+
+
+def some_query():
+    QUERY = f"""SELECT Surname, Name
+                FROM Doctors
+                WHERE Premium > SOME (
+                SELECT AVG(Premium)
+                FROM Doctors);"""
+    return QUERY
+
+
+"""Врачи, у которых зарплата выше, чем у всех врачей в отделе "Кардиология"."""
+
+
+def all_query():
+    QUERY = f"""SELECT Surname, Name
+                FROM Doctors
+                WHERE Salary > ALL (
+                SELECT Salary
+                FROM Doctors
+                WHERE DepartmentID = (
+                SELECT Id
+                FROM Departments
+                WHERE DepartmentName = 'Кардиология'));"""
+    return QUERY
+
+
+"""ANY/SOME ALL. Врачи, у которых зарплата выше, чем у всех врачей в отделе "Кардиология", но ниже, чем у любого врача в отделе "Хирургия"."""
+
+
+def any_some_all_query():
+    QUERY = f"""SELECT Surname, Name
+                FROM Doctors
+                WHERE Salary > ALL (SELECT Salary
+                                    FROM Doctors
+                                    WHERE DepartmentID = (SELECT Id
+                                                            FROM Departments
+                                                            WHERE DepartmentName = 'Кардиология'))
+                AND Salary < ANY (SELECT Salary
+                                    FROM Doctors
+                                    WHERE DepartmentID = (SELECT Id
+                                                            FROM Departments
+                                                            WHERE DepartmentName = 'Педиатрия'));"""
+    return QUERY
+
+
+"""UNION. Список врачей из отдела "Кардиология" и "Хирургия"""
+
+
+def union_query():
+    QUERY = f"""SELECT Surname, Name
+                FROM Doctors
+                WHERE DepartmentID = (
+                    SELECT Id
+                    FROM Departments
+                    WHERE DepartmentName = 'Хирургия'
+                )
+                UNION
+                SELECT Surname, Name
+                FROM Doctors
+                WHERE DepartmentID = (
+                    SELECT Id
+                    FROM Departments
+                    WHERE DepartmentName = 'Неврология');"""
+    return QUERY
+
+
+"""UNION ALL. Список врачей из отдела "Кардиология" и "Хирургия", включая все дубликаты."""
+
+
+def union_all_query():
+    QUERY = f"""SELECT Surname, Name
+                FROM Doctors
+                WHERE DepartmentID = (
+                    SELECT Id
+                    FROM Departments
+                    WHERE DepartmentName = 'Неврология'
+                )
+                UNION ALL
+                SELECT Surname, Name
+                FROM Doctors
+                WHERE DepartmentID = (
+                    SELECT Id
+                    FROM Departments
+                    WHERE DepartmentName = 'Терапия');"""
+    return QUERY
+
+
+"""INNER JOIN. список врачей с их отделами."""
+
+
+def inner_join():
+    QUERY = f"""SELECT D.Surname, D.Name, DE.DepartmentName
+                FROM Doctors AS D
+                INNER JOIN Departments AS DE ON D.DepartmentID = DE.Id;"""
+    return QUERY
+
+
+"""LEFT JOIN. Список всех отделов и соответствующих врачей, включая отделы без врачей."""
+
+
+def left_join():
+    # join_dict = {}
+    QUERY = f"""SELECT Departments.DepartmentName, Doctors.Surname, Doctors.Name
+                FROM Departments
+                LEFT JOIN Doctors ON Departments.Id = Doctors.DepartmentID;
+                """
+    # join_dict = {'DepName':QUERY.DepartmentName,
+    #              'Surname':QUERY.Surname,
+    #              'Name':QUERY.Name}
+    # with open('left_query.json', 'w',encoding='UTF-8') as f:
+    #     json.dump(join_dict, f, ensure_ascii=False, indent=4)
+    return QUERY
+
+"""RIGHT JOIN. список всех врачей и их отделов, включая врачей без отделов."""
+def right_join():
+    QUERY = f"""SELECT Doctors.Surname, Doctors.Name, Departments.DepartmentName
+                FROM Doctors
+                RIGHT JOIN Departments ON Doctors.DepartmentID = Departments.Id;"""
     return QUERY
